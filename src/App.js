c6e31d95e99d1616088f66c36c048c94426184ba
Jai indra <168549352+Jaiindra1@users.noreply.github.com>
2025-03-14 22:12:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import Receipt from './components/Reciept/index.js';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ProductSelect from './components/Product Selection/index.js';
+import {BrowserRouter as Router} from "react-router-dom";
+import Dashboard from './components/Dashboard/index.js';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route  path='/' Component={ProductSelect}/>
-          <Route  path='/Receipt' Component={Receipt}/>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+    <Dashboard />
+</Router>
   )
 }
 
