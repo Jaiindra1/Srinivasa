189a35fb21ca5fbc9fd4e07b10cf99e056de6726
Jai indra <168549352+Jaiindra1@users.noreply.github.com>
2025-03-26 22:01:55 +0530

    Update index.js

--- src/components/Reciept/index.js
+++ src/components/Reciept/index.js
@@ -82,7 +82,7 @@ useEffect(() => {
         };
 
         try {
-            await fetch("http://localhost:5000/save-bill", {
+            await fetch("https://srinivasa-backend.onrender.com/save-bill", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(billData),
